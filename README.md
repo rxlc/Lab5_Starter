894c52b63d16540432031a888b2b7288ddfc63cd
rxlc <110650273+rxlc@users.noreply.github.com>
2026-05-03 04:21:34 -0700

    testing branch: should be wrong

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 Name: Quinton Fu
 
 Expose: https://rxlc.github.io/Lab5_Starter/expose.html
+
 Explore: https://rxlc.github.io/Lab5_Starter/explore.html
+
+1. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+No, since this process would involve accessing databases, servers, and doing asychronious requests, and unit tests should only focus on a function/implementation in isolation
+
+2. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+Yes, since it doesn't need to use other systems like databases and servers, and runs in insolation.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -6,6 +6,89 @@ import {
   isStrongPassword,
   isDate,
   isHexColor,
-} from '../code-to-unit-test/unit-test-me';
+} from "../code-to-unit-test/unit-test-me";
 
-// TODO - Part 2
+//Testing isPhoneNumber
+test("The standard format (xxx) xxx-xxx should pass", () => {
+  expect(isPhoneNumber("(754) 298-3021")).toBe(true);
+});
+
+test("No parentheses xxx-xxx-xxx should pass", () => {
+  expect(isPhoneNumber("255-214-5943")).toBe(true);
+});
+
+test("No seperators xxxxxxxxx should fail", () => {
+  expect(isPhoneNumber("8842153348")).toBe(false);
+});
+
+test("Wrong hypens xx-xxxxx-xxx should fail", () => {
+  expect(isPhoneNumber("33-27414-124")).toBe(false);
+});
+
+//Testing isEmail
+test("Standard format should pass", () => {
+  expect(isEmail("test@ucsd.edu")).toBe(true);
+});
+
+test("Underscore included should pass", () => {
+  expect(isEmail("test_123@ucsd.edu")).toBe(true);
+});
+
+test("Missing prefix should fail", () => {
+  expect(isEmail("@ucsd.edu")).toBe(false);
+});
+
+test("Missing domain should fail", () => {
+  expect(isEmail("test@.edu")).toBe(false);
+});
+
+//Testing isStrongPassword
+test("First character is letter followed by 3 numbers should pass", () => {
+  expect(isStrongPassword("b205")).toBe(true);
+});
+
+test("First character is letter followed by number, letter, and underscores (length 15) should pass", () => {
+  expect(isStrongPassword("A12_45_q32_88_a")).toBe(true);
+});
+
+test("Exclamation mark somewhere after the first letter should fail", () => {
+  expect(isStrongPassword("o129!flkm")).toBe(false);
+});
+
+test("First character is a number should fail", () => {
+  expect(isStrongPassword("5ackmpo9")).toBe(false);
+});
+
+// Testing isDate
+test("Standard date xx/xx/xxxx should pass", () => {
+  expect(isDate("12/25/2025")).toBe(true);
+});
+
+test("Single digit x/x/xxxx should fail", () => {
+  expect(isDate("1/2/2019")).toBe(true);
+});
+
+test("Two digit year x/x/xxxx should fail", () => {
+  expect(isDate("5/6/22")).toBe(false);
+});
+
+test("Wrong seperator xx-xx-xxxx should fail", () => {
+  expect(isDate("6-8-2026")).toBe(false);
+});
+
+// Testing isHexColor
+test("Standard hex with hash should pass", () => {
+  expect(isHexColor("#FAC25F")).toBe(true);
+});
+
+test("3 characters with no hash should pass", () => {
+  expect(isHexColor("3AB")).toBe(true);
+});
+
+test("2 characters with hash should fail", () => {
+  expect(isHexColor("#AA")).toBe(false);
+});
+
+test("Invalid hex letters should fail", () => {
+  expect(isHexColor("#ABCXYZ")).toBe(false);
+});
