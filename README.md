7171f6736efca745759f41d7c4ef585f96c0b80c
rxlc <110650273+rxlc@users.noreply.github.com>
2026-05-03 02:22:21 -0700

    Implemented explore.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Lab 5 - Starter
-Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
+Name: Quinton Fu
+
+Expose: https://rxlc.github.io/Lab5_Starter/expose.html
+Explore: https://rxlc.github.io/Lab5_Starter/explore.html
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,47 @@
 // explore.js
-
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+  let talkButtonElement = document.querySelector("#explore > button");
+  let faceImageElement = document.querySelector("#explore > img");
+  let voicesDropdown = document.getElementById("voice-select");
+  let textToSpeakElement = document.getElementById("text-to-speak");
+
+  let voicesList = [];
+
+  function populateVoices() {
+    voicesList = synth.getVoices();
+
+    for (let i = 0; i < voicesList.length; i++) {
+      let option = document.createElement("option");
+      option.value = i;
+      option.textContent = voicesList[i].name;
+
+      voicesDropdown.appendChild(option);
+    }
+  }
+
+  talkButtonElement.addEventListener("click", () => {
+    let selectedVoice = voicesList[voicesDropdown.value];
+
+    if (selectedVoice) {
+      let utterance = new SpeechSynthesisUtterance(textToSpeakElement.value);
+      utterance.voice = selectedVoice;
+
+      utterance.addEventListener("start", (event) => {
+        faceImageElement.src = "assets/images/smiling-open.png";
+        faceImageElement.alt = "Smiling open face";
+      });
+
+      utterance.addEventListener("end", (event) => {
+        faceImageElement.src = "assets/images/smiling.png";
+        faceImageElement.alt = "Smiling face";
+      });
+
+      speechSynthesis.speak(utterance);
+    }
+  });
+
+  window.speechSynthesis.addEventListener("voiceschanged", populateVoices);
 }
